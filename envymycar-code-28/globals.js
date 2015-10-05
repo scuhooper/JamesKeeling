2905bf80a46277aa72318a3c61612221fdb572b7
scuhooper <keeling18@marshall.edu>
2015-10-05 13:40:59 -0400

    remote_media URL
    fix URL

--- envymycar-code-28/globals.js
+++ envymycar-code-28/globals.js
@@ -5,4 +5,4 @@
 
 // If you do not  have a local web server and just open the html with a browser,
 // then uncomment the following line
-NVMC.resource_path = "http://scuhooper.github.io/mygithubpage/envymycar-code-28/media_remote"
+NVMC.resource_path = "http://scuhooper.github.io/mygithubpage/envymycar-code-28/media_remote/"
